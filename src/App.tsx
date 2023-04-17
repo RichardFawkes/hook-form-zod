49498d2510f72a9afe36b95ae72abfbcd3be2fa9
Richard <richarddevelopers@gmail.com>
2023-04-16 22:05:04 -0300

    feat: field arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./styles/global.css";
 
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
@@ -26,6 +26,14 @@ const createUserFormSchema = z.object({
       return email.endsWith("@gmail.com");
     }, "O e-mail precisa ser do Gmail"),
   password: z.string().min(6, "A senha precisa de no mínimo de 6 caracteres"),
+  techs: z
+    .array(
+      z.object({
+        title: z.string().nonempty("O titulo e obrigatorio"),
+        knowledge: z.coerce.number().min(1).max(100),
+      })
+    )
+    .min(2, "Insira pelo menos 2 tecnlogias"),
 });
 
 type CreateUserFormData = z.infer<typeof createUserFormSchema>;
@@ -35,10 +43,20 @@ export function App() {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<CreateUserFormData>({
     resolver: zodResolver(createUserFormSchema),
   });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "techs",
+  });
+
+  function addNewTech() {
+    append({ title: "", knowledge: 0 });
+  }
 
   function createUser(data: any) {
     setOutput(JSON.stringify(data, null, 2));
@@ -57,7 +75,9 @@ export function App() {
             className=" border border-zinc-600 shadow-sm rounded h-10 px-3 text-white bg-zinc-900"
             {...register("name")}
           />
-          {errors.name && <span>{errors.name.message}</span>}
+          {errors.name && (
+            <span className="text-red-500 text-sm">{errors.name.message}</span>
+          )}
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
@@ -66,23 +86,72 @@ export function App() {
             className=" border border-zinc-600 shadow-sm rounded h-10 px-3 text-white bg-zinc-900"
             {...register("email")}
           />
-          {errors.email && <span>{errors.email.message}</span>}
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email.message}</span>
+          )}
         </div>
 
         <div className="flex flex-col gap-1">
           <label htmlFor="">Senha</label>
-          <input
-            type="password"
-            className=" border border-zinc-600 shadow-sm rounded h-10 px-3 text-white bg-zinc-900"
-            {...register("password")}
-          />
-          {errors.password && <span>{errors.password.message}</span>}
+
+          {errors.password && (
+            <span className="text-red-500 text-sm">
+              {errors.password.message}
+            </span>
+          )}
         </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="" className="flex items-center justify-between">
+            Tecnologias
+            <button
+              type="button"
+              onClick={addNewTech}
+              className="text-emerald-500 text-sm"
+            >
+              Adicionar
+            </button>
+          </label>
+          {/* {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>} */}
+          {fields.map((fields, index) => {
+            return (
+              <div className="flex flex-1 gap-2" key={fields.id}>
+                <div className="flex-1 flex-col gap-1">
+                  <input
+                    type="text"
+                    className=" border border-zinc-600 shadow-sm rounded h-10 px-3 text-white bg-zinc-900"
+                    {...register(`techs.${index}.title`)}
+                  />
+                  {errors.techs?.[index]?.title && (
+                    <span className="text-red-500 text-sm">
+                      {errors.techs?.[index]?.title?.message}
+                    </span>
+                  )}
+                </div>
+                <div className="flex-1 flex-col gap-1">
+                  <input
+                    type="number"
+                    className="w-16 flex-1 border border-zinc-600 shadow-sm rounded h-10 px-3 text-white bg-zinc-900"
+                    {...register(`techs.${index}.knowledge`)}
+                  />
+                  {errors.techs?.[index]?.knowledge && (
+                    <span className="text-red-500 text-sm">
+                      {errors.techs?.[index]?.knowledge?.message}
+                    </span>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+          {errors.techs && (
+            <span className="text-red-500 text-sm">{errors.techs.message}</span>
+          )}
+        </div>
+
         <button
           type="submit"
           className="bg-emerald-500 rounded font-semibold text-white h-10"
         >
-          Entrar
+          Salvar
         </button>
         <pre className=" text-white h-10">{output}</pre>
       </form>
